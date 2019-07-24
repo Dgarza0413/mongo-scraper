e919626e88060a68bd2dbc1c99009bccc90e105d
David Garza <dgarza0413@gmail.com>
2019-07-23 23:10:12 -0500

    html routes in place

---
+++ server.js
@@ -0,0 +1,64 @@
+var express = require("express");
+var mongojs = require("mongojs");
+var axios = require("axios");
+var cheerio = require("cheerio");
+var path = require("path")
+
+var PORT = process.env.PORT || 3000
+
+var app = express();
+var databaseUrl = "news";
+var collections = ["media"];
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+
+var db = mongojs(databaseUrl, collections);
+db.on("error", (error) => {
+    console.log("Database Error:", error);
+});
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "./public/landing.html"));
+})
+
+app.get("/all", (req, res) => {
+    db.media.find({}, (error, found) => {
+        if (error) {
+            console.log(error)
+            res.status(500).send(error);
+        } else {
+            res.json(found);
+        }
+    })
+})
+
+app.get("/scrap", (req, res) => {
+    res.send("this the scraped page")
+    axios.get("http://www.apnews.com").then((response) => {
+        var $ = cheerio.load(response.data);
+        var results = [];
+        $("article").each(function (i, element) {
+            var title = $(element).children().text();
+            var link = $(element).find("a").attr("href");
+            if (title && link) {
+                db.media.insert({
+                    title,
+                    link
+                }, function (err, inserted) {
+                    if (err) {
+                        console.log(err);
+                    } else {
+                        console.log("inserted instead")
+                    }
+                })
+            }
+            console.log(results)
+        });
+    })
+})
+
+app.listen(3000, () => {
+    console.log("app is on http://localhost:" + PORT);
+})
